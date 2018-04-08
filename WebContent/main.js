497646b5a1df4cc791ba06c2d11c04d9f423fe50
kobataku <tk_musik@yahoo.co.jp>
2018-04-08 15:33:26 +0900

    結果を表に

--- WebContent/main.js
+++ WebContent/main.js
@@ -197,12 +197,83 @@ async function requestCalc(arg, worktype) {
             alert("worktypeがおかしいんじゃ:" + worktype);
     }
     time = Date.now() - time;
-    resultOutPut(result, time, worktype);
+    pushResult(result, time, worktype);
+    resultOutPut();
 }
-function resultOutPut(result, ms, worktype) {
-    const resultstr = result ? "worktype:" + WorkType[worktype] + "<br>time:" + ms + "ms" + "<br>linenum:" + result.lineNum + "<br>ave:" + result.val + "<br>nodata:" + result.noDataNum + "<br>invalidData:" + result.invalidDataNum : "null!";
+function pushResult(result, ms, worktype) {
+    if (!window.resultsForTable) {
+        window.resultsForTable = new Results(10);
+    }
+    window.resultsForTable.push({ result, ms }, worktype);
+}
+function resultOutPut() {
+    const table = document.createElement("table");
+    const tr_header = document.createElement("tr");
+    const kind = document.createElement("th");
+    kind.innerHTML = "種別";
+    tr_header.appendChild(kind);
+    for (let i = 0; i < (window.resultsForTable ? window.resultsForTable.maxLength : 0); i++) {
+        const header = document.createElement("th");
+        header.innerHTML = (i + 1).toString();
+        tr_header.appendChild(header);
+    }
+    table.appendChild(tr_header);
+    const tr_normal = document.createElement("tr");
+    const tr_worker = document.createElement("tr");
+    const th_normalTitle = document.createElement("th");
+    th_normalTitle.innerHTML = "フツーのfor文";
+    tr_normal.appendChild(th_normalTitle);
+    const th_workerTitle = document.createElement("th");
+    th_workerTitle.innerHTML = "WebWorker";
+    tr_worker.appendChild(th_workerTitle);
+    table.appendChild(tr_normal);
+    table.appendChild(tr_worker);
+    const normalResult = window.resultsForTable.result[WorkType.normal];
+    const workerResult = window.resultsForTable.result[WorkType.webworker];
+    for (let i = 0, l = window.resultsForTable.maxLength; i < l; i++) {
+        let normalTH = document.createElement("th");
+        normalTH.innerHTML = "null";
+        let workerTH = document.createElement("th");
+        workerTH.innerHTML = "null";
+        if (normalResult[i] && normalResult[i].result) {
+            const normalSubResult = normalResult[i].result;
+            const tooltipStr = "linenum:" + normalSubResult.lineNum + "\nave:" + normalSubResult.val + "\nnodata:" + normalSubResult.noDataNum + "\ninvalidData:" + normalSubResult.invalidDataNum;
+            normalTH.innerHTML = normalResult[i].ms + "ms";
+            normalTH.title = tooltipStr;
+        }
+        if (workerResult[i] && workerResult[i].result) {
+            const workerSubResult = workerResult[i].result;
+            const tooltipStr = "linenum:" + workerSubResult.lineNum + "\nave:" + workerSubResult.val + "\nnodata:" + workerSubResult.noDataNum + "\ninvalidData:" + workerSubResult.invalidDataNum;
+            workerTH.innerHTML = workerResult[i].ms + "ms";
+            workerTH.title = tooltipStr;
+        }
+        tr_normal.appendChild(normalTH);
+        tr_worker.appendChild(workerTH);
+    }
     const d = document.getElementById("calc-result");
-    d.innerHTML = resultstr;
+    d.innerHTML = "";
+    d.appendChild(table);
+}
+class Results {
+    constructor(ml) {
+        this.maxLength = ml;
+        this.result = {};
+        for (let v in WorkType) {
+            const t = parseInt(v);
+            if (!isNaN(t)) {
+                this.result[t] = [];
+            }
+        }
+    }
+    push(result, worktype) {
+        if (!this.result[worktype]) {
+            alert("Results.pushできません。worktypeがおかしいです。：" + worktype);
+        }
+        this.result[worktype].unshift(result);
+        if (this.result[worktype].length > this.maxLength) {
+            this.result[worktype] = this.result[worktype].slice(0, this.maxLength);
+        }
+    }
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import * as calc from "./calc";
 import { NoDataTreat as NDT, CalcArg as CArg, CalcResult as CRslt } from "./calc";
 
+declare global {
+    interface Window { resultsForTable: Results; }
+}
+
 enum WorkType {
     webworker,
     normal
@@ -39,11 +43,91 @@ async function requestCalc(arg: CArg, worktype: WorkType) {
             alert("worktypeがおかしいんじゃ:" + worktype);
     }
     time = Date.now() - time;
-    resultOutPut(result, time, worktype);
+    pushResult(result, time, worktype);
+    resultOutPut();
 }
 
-function resultOutPut(result: CRslt, ms: number, worktype: WorkType) {
-    const resultstr = result ? "worktype:" + WorkType[worktype] + "<br>time:" + ms + "ms" + "<br>linenum:" + result.lineNum + "<br>ave:" + result.val + "<br>nodata:" + result.noDataNum + "<br>invalidData:" + result.invalidDataNum : "null!";
+function pushResult(result: CRslt, ms: number, worktype: WorkType) {
+    if (!window.resultsForTable) {
+        window.resultsForTable = new Results(10);
+    }
+    window.resultsForTable.push({ result, ms }, worktype);
+}
+
+/** 結果を表示するDOM職人 */
+function resultOutPut() {
+    const table = document.createElement("table");
+    const tr_header = document.createElement("tr");
+    const kind = document.createElement("th");
+    kind.innerHTML = "種別";
+    tr_header.appendChild(kind);
+    for (let i = 0; i < (window.resultsForTable ? window.resultsForTable.maxLength : 0); i++) {
+        const header = document.createElement("th");
+        header.innerHTML = (i + 1).toString();
+        tr_header.appendChild(header);
+    }
+    table.appendChild(tr_header);
+
+    const tr_normal = document.createElement("tr");
+    const tr_worker = document.createElement("tr");
+    const th_normalTitle = document.createElement("th");
+    th_normalTitle.innerHTML = "フツーのfor文";
+    tr_normal.appendChild(th_normalTitle);
+    const th_workerTitle = document.createElement("th");
+    th_workerTitle.innerHTML = "WebWorker";
+    tr_worker.appendChild(th_workerTitle);
+
+    table.appendChild(tr_normal);
+    table.appendChild(tr_worker);
+
+    const normalResult = window.resultsForTable.result[WorkType.normal];
+    const workerResult = window.resultsForTable.result[WorkType.webworker];
+    for (let i = 0, l = window.resultsForTable.maxLength; i < l; i++) {
+        let normalTH = document.createElement("th");
+        normalTH.innerHTML = "null";
+        let workerTH = document.createElement("th");
+        workerTH.innerHTML = "null";
+        if (normalResult[i] && normalResult[i].result) {
+            const normalSubResult = normalResult[i].result;
+            const tooltipStr = "linenum:" + normalSubResult.lineNum + "\nave:" + normalSubResult.val + "\nnodata:" + normalSubResult.noDataNum + "\ninvalidData:" + normalSubResult.invalidDataNum;
+            normalTH.innerHTML = normalResult[i].ms + "ms";
+            normalTH.title = tooltipStr;
+        }
+        if (workerResult[i] && workerResult[i].result) {
+            const workerSubResult = workerResult[i].result;
+            const tooltipStr = "linenum:" + workerSubResult.lineNum + "\nave:" + workerSubResult.val + "\nnodata:" + workerSubResult.noDataNum + "\ninvalidData:" + workerSubResult.invalidDataNum;
+            workerTH.innerHTML = workerResult[i].ms + "ms";
+            workerTH.title = tooltipStr;
+        }
+        tr_normal.appendChild(normalTH);
+        tr_worker.appendChild(workerTH);
+    }
+
     const d = <HTMLDivElement>document.getElementById("calc-result");
-    d.innerHTML = resultstr;
+    d.innerHTML = "";
+    d.appendChild(table);
+}
+
+class Results {
+    result: { [worktype: number]: { result: CRslt, ms: number }[] };
+    maxLength: number;
+    constructor(ml: number) {
+        this.maxLength = ml;
+        this.result = {};
+        for (let v in WorkType) {
+            const t = parseInt(v);
+            if (!isNaN(t)) {
+                this.result[t] = [];
+            }
+        }
+    }
+    push(result: { result: CRslt, ms: number }, worktype: WorkType) {
+        if (!this.result[worktype]) {
+            alert("Results.pushできません。worktypeがおかしいです。：" + worktype);
+        }
+        this.result[worktype].unshift(result);
+        if (this.result[worktype].length > this.maxLength) {
+            this.result[worktype] = this.result[worktype].slice(0, this.maxLength);
+        }
+    }
 }
